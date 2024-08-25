84ea057b05d5441f4bbb948e6688fcc3e2b7c9b7
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-08-24 20:11:30 -0600

    actualizar descripcion de los servicios en constans/text.js

--- src/constants/texts.js
+++ src/constants/texts.js
@@ -2,57 +2,52 @@ const serviciosOfrecidos = {
   // Servicios
   manicuraCombinada: {
     servicio: "Manicura Combinada",
-    descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Veilt totam blanditis iste excerciatiomem temporibus minus natus dignissismos aliquam minima evertir.",
+    descripcion: "Manicura profunda en seco con drill, puntas para cada tipo de piel y tijeras. En esta limpieza, se retira cuticula, limpian laterales y se pule la piel para un mejor acabado.",
     precio: "Precio base: $130 (varía según el diseño)."
   },
   manicuraSpa: {
     servicio: "Manicura spa",
-    descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Veilt totam blanditis iste excerciatiomem temporibus minus natus dignissismos aliquam minima evertir.",
+    descripcion: "Limpieza profunda complementada con una bandeja de sales minerales, exfoliación de manos con masaje e hidratación.",
     precio: "Precio fijo: $150"
   },
   gelSemipermanente: {
     servicio: "Gel semipermanente rubber",
-    descripcion: "Des",
+    descripcion: "Esmaltado sobre uña natural que incluye base rubber para nivelar y reforzar la uña. Incluye manicura combinada y esmaltado, que puede ser en tono liso o con diseño (diseño a cotizar).",
     precio: "$200-250"
   },
   nivelacion: {
     servicio: "Nivelación",
-    descripcion: "Des",
+    descripcion: "Con la ayuda de Rubber o gel de construcción, se recubre, refuerza y protege la uña natural, corrigiendo pequeñas hendiduras o irregularidades. Incluye manicura combinada y gel semipermanente en tono liso o con diseño (diseño a cotizar).",
     precio: "$250-300"
   },
   banioAcrilico: {
-    servicio: "Baño de acrílico",
-    descripcion: "Des",
+    servicio: "Baño (kapping) de acrílico o polygel",
+    descripcion: "Ideal para reforzar uñas naturales medianas o largas. Incluye manicura combinada. El diseño debe ser cotizado con anticipación.",
     precio: "Apartir $280"
   },
   uniasAcrilicas: {
     servicio: "Uñas acrílicas",
-    descripcion: "Des",
+    descripcion: "Extensión de largo a elección de la clienta, que puede ser mediante tip o técnica escultural, según el estado, condición y largo de la uña natural. Incluye manicura combinada. El diseño debe ser cotizado con anticipación.",
     precio: "Apartir $300"
   },
   uniasSoftGel: {
     servicio: "Uñas Soft gel",
-    descripcion: "Des",
+    descripcion: "Extensión ligera y resistente de gel. Incluye manicura combinada. El diseño debe ser cotizado con anticipación.",
     precio: "Apartir $280"
   },
-  esmaltadoPies: {
-    servicio: "Esmaltado en pies",
-    descripcion: "Des",
-    precio: "$200"
-  },
   pedicuraSeco: {
-    servicio: "Pedicura en seco con gel",
-    descripcion: "Des",
+    servicio: "Pedicura en seco",
+    descripcion: "Pedicura estética en seco (sin uso de tina con agua), que incluye limpieza de las uñas con drill, puntas y alicate, corte y limado de uñas, eliminación de hiperqueratosis (callosidades), exfoliación e hidratación. Incluye gel semipermanente en un tono.",
     precio: "$300"
   },
   pedicuraSpa: {
     servicio: "Pedicura Spa con gel",
-    descripcion: "Des",
+    descripcion: "Pedicura estética que incluye limpieza de las uñas con drill, puntas y alicate, corte y limado de uñas, así como un baño en tina con agua tibia, sales minerales y bomba efervescente. Se realiza también la eliminación de hiperqueratosis (callosidades), exfoliación e hidratación. Incluye gel semipermanente en un tono.",
     precio: "$380"
   },
-  pedicuraAdvance: {
-    servicio: "Pedicura Advance",
-    descripcion: "Des",
+  extensionUngleal: {
+    servicio: "Extensión ungueal",
+    descripcion: "Consiste en realizar la cuadratura de la uña utilizando polygel. Este proceso incluye el moldeado y ajuste de la forma de la uña para lograr un acabado rectangular y uniforme, proporcionando una estructura sólida y duradera. El polygel se aplica para reforzar y definir la forma deseada, ofreciendo un acabado estético y resistente.",
     precio: "$450"
   }
 }
